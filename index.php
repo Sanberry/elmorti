8ee49f90c664c265c0af96ab44bea5a6d6e47f5d
Sanberry <32483996+Sanberry@users.noreply.github.com>
2017-10-10 13:38:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 echo "<h1>Hello Samuelito</h1>";
 echo "<h2>Check it out</h2>";
-echo "<p><a href='https://saydemo001.herokuapp.com/jsonapi.php'>Link to Saydemo001</p>";
+echo "<p><a href='lemonparty.org'>Link to demo1</p>";
