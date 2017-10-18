74b81a60ffb8a83fc93cac17531876c6f09cca2a
Sanberry <32483996+Sanberry@users.noreply.github.com>
2017-10-18 16:43:53 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+
+//trial I have no ide what is going on..
+
+<link href="stylesheets/jquery.cssemoticons.css" media="screen" rel="stylesheet" type="text/css" />
+<script src="javascripts/jquery.js" type="text/javascript"></script>
+<script src="javascripts/jquery.cssemoticons.js" type="text/javascript"></script>
+  
 echo "<h1 style=color:purple;> Hello Samuelito! </h1>";
-echo "<h2 style=color:blue;> Check it out str_replace(';)' <h2>";
+echo "<h2 style=color:blue;> Check it out <h2>";
 echo "<p><a href='https://sanemadoressamuel.wordpress.com/'>Link to elmorti</p>";
