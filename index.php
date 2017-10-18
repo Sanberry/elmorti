5a1af2f20e186d56e61fab57d4a69db8e2ff92c3
Sanberry <32483996+Sanberry@users.noreply.github.com>
2017-10-18 16:20:47 -0700

    colour change

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "<h1> \033[01;31m Hello Samuelito |033[0m </h1>";
+echo "<h1> \033[01;31m Hello Samuelito \033[0m </h1>";
 echo "<h2>Check it out</h2>";
 echo "<p><a href='https://sanemadoressamuel.wordpress.com/'>Link to elmorti</p>";
