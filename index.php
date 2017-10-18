13f08c7cb9a1aedfdfdd0c66458ca3d720bb9cb1
Sanberry <32483996+Sanberry@users.noreply.github.com>
2017-10-18 16:29:26 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 echo "<h1 style=color:purple;> Hello Samuelito </h1>";
-echo "<p style=color:light pink>Check it out</p>";
+echo "<h2 style=color:205>Check it out<h2>";
 echo "<p><a href='https://sanemadoressamuel.wordpress.com/'>Link to elmorti</p>";
